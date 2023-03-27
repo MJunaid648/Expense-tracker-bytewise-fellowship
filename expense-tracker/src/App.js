f6a70fdd56c8da1fbb209b7f2c88de27fb905dff
Muhammad Junaid <648mjunaid@gmail.com>
2023-03-28 00:56:49 +0500

    completed

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import HomeComponent from "./modules/home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Expense Tracker</h1>
+      <HomeComponent />
+    </div>
+  );
+}
+
+export default App;
